b93336a52f9dd19c5f9e4ac5344760bf55ad4bfb
Rohan Sakhale <rohansakhale@gmail.com>
2022-01-02 21:41:34 +0530

    Deploy script update

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,5 +4,8 @@
     cd /home/ec2-user/sites/label-generator.saiashirwad.com
     git pull origin main
     composer install
+    php artisan down
     php artisan migrate --force
+    php artisan queue:restart
+    php artisan up
 @endtask
