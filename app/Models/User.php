4f30da8bcf8bfbf964a71c056b0d4a9d24e91506
Rohan Sakhale <rohansakhale@gmail.com>
2025-09-13 16:30:35 +0530

    Update

--- app/Models/User.php
+++ app/Models/User.php
@@ -145,11 +145,6 @@ class User extends Authenticatable
         return $this->level == 'admin';
     }
 
-    public function setPasswordAttribute($value)
-    {
-        $this->attributes['password'] = bcrypt($value);
-    }
-
     public function templates()
     {
         return $this->hasMany(Template::class);
